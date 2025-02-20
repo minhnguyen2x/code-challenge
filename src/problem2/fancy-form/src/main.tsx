61b8299bc789026f0b462ff26d925b808a6aaaaf
Minh Nguyen <nguyenthanhminh112000@gmail.com>
2025-02-20 23:13:57 +0700

    chore(PRB-000002): set up material ui

--- src/problem2/fancy-form/src/main.tsx
+++ src/problem2/fancy-form/src/main.tsx
@@ -1,9 +1,15 @@
 import { Currency } from '@features/currency-exchange/components/currency';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline />
     <div>Fancy Form</div>
     <Currency />
   </StrictMode>
